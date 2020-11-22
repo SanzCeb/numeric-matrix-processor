f02706a996055242646b2bc638abf6c0288e84de
Sergio <sanz.cebollero@zoho.com>
2020-11-22 15:23:52 +0100

    Stage4 (#4)
    * Stage 4/6: Matrix transpose completed.
    
    Co-authored-by: Sergio Sanz Cebollero <scebollero@guidewire.com>

--- processor/client/NumericMatrixProcessor.java
+++ processor/client/NumericMatrixProcessor.java
@@ -4,18 +4,18 @@ import processor.NumericMatrix;
 import processor.NumericMatrixReader;
 
 import java.util.Scanner;
+import java.util.function.Function;
+
 
 public class NumericMatrixProcessor {
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public static void run() {
-
         try {
             runNumericMatrixProcessorMenu();
         }catch (Exception ignored) {
             System.out.println("ERROR");
         }
-
     }
 
     static void runNumericMatrixProcessorMenu() {
@@ -28,66 +28,103 @@ public class NumericMatrixProcessor {
                 case 0:
                     break;
                 case 1:
-                    runAddMatrices();
+                    runBiMatrixOperator("add");
                     break;
                 case 2:
                     runMulMatricesByAConst();
                     break;
                 case 3:
-                    runMulMatrices();
+                    runBiMatrixOperator("multiply");
+                    break;
+                case 4:
+                    runTranspose();
+                    break;
+                default:
+                    choice = -1;
+                    break;
+            }
+        } while(choice != 0);
+    }
+
+    private static void runTranspose() {
+        int choice;
+        do {
+            printTransposeMenu();
+            System.out.print("Your choice: ");
+            choice = Integer.parseInt(SCANNER.nextLine());
+            switch (choice) {
+                case 0:
+                    break;
+                case 1:
+                    transposeMatrix(NumericMatrix::transposeMainDiagonal);
+                    break;
+                case 2:
+                    transposeMatrix(NumericMatrix::transposeSideDiagonal);
+                    break;
+                case 3:
+                    transposeMatrix(NumericMatrix::transposeVerticalLine);
+                    break;
+                case 4:
+                    transposeMatrix(NumericMatrix::transposeHorizontalLine);
                     break;
                 default:
                     choice = -1;
                     break;
             }
 
-        } while(choice != 0);
+        } while(choice == -1);
     }
 
-    private static void runMulMatrices() {
+    private static void transposeMatrix(Function<NumericMatrix, NumericMatrix> transposeOperator) {
         try {
-
-            NumericMatrix matrixA = NumericMatrixReader.readNumericMatrix(SCANNER, " first");
-            NumericMatrix matrixB = NumericMatrixReader.readNumericMatrix(SCANNER, " second");
+            NumericMatrix matrixA = NumericMatrixReader.readNumericMatrix(SCANNER, "");
             System.out.println("The result is:");
-            System.out.print(matrixA.multiply(matrixB));
+            System.out.print(transposeOperator.apply(matrixA));
 
         } catch (Exception exception) {
             System.out.println(exception.getMessage());
         }
     }
 
-    private static void runAddMatrices() {
+    private static void runBiMatrixOperator(String matrixOperator) {
         try {
+
             NumericMatrix matrixA = NumericMatrixReader.readNumericMatrix(SCANNER, " first");
             NumericMatrix matrixB = NumericMatrixReader.readNumericMatrix(SCANNER, " second");
             System.out.println("The result is:");
-            System.out.print(matrixA.add(matrixB));
+            var numericMatrixClass = NumericMatrix.class;
+            var matrixOperatorMethod = numericMatrixClass.getMethod(matrixOperator, numericMatrixClass);
+            System.out.println(matrixOperatorMethod.invoke(matrixA, matrixB));
 
         } catch (Exception exception) {
             System.out.println(exception.getMessage());
         }
-
     }
 
     private static void runMulMatricesByAConst() {
         try {
-
             NumericMatrix matrixA = NumericMatrixReader.readNumericMatrix(SCANNER, "");
             var scalar = SCANNER.nextInt();
             System.out.println("The result is:");
             System.out.print(matrixA.mulByScalar(scalar));
-
         } catch (Exception exception) {
             System.out.println(exception.getMessage());
         }
 
+    }
+
+    private static void printTransposeMenu() {
+        System.out.println("1. Main diagonal");
+        System.out.println("2. Side diagonal");
+        System.out.println("3. Vertical line");
+        System.out.println("4. Horizontal line");
     }
 
     private static void printMenu() {
         System.out.println("\n1. Add matrices");
         System.out.println("2. Multiply matrix by a constant");
         System.out.println("3. Multiply matrices");
+        System.out.println("4. Transpose matrices");
         System.out.println("0. Exit");
     }
 }
